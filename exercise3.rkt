1566efd7cfc4b9d19174b63db78af29d7c90aac6
Lis Voeal <voealis@outlook.com>
2022-11-08 16:56:03 +0300

    finished ex3

---
+++ exercise3.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define str "helloworld")
+(define i 5)
+(define (add-underscore string position)
+  (string-append (substring string 0 position) "_" (substring string position)))
+
+(add-underscore str i)
