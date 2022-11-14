99c44bd8e0659914e894b618f0d6ffb91a574388
Lis Voeal <voealis@outlook.com>
2022-11-14 15:16:44 +0300

    finished ex7

---
+++ exercise7.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise7) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define sunny #true)
+(define friday #false)
+(define (calculate x y)
+  (and (and x #false)
+       (and y #true)))
+(calculate sunny friday)
