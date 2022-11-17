4c62b71510cc815c3e0d69e86f10a03e7d94a0ae
Lis Voeal <voealis@outlook.com>
2022-11-17 08:57:20 +0300

    finished ex11 ex12 ex13

---
+++ exercise13.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise13) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (string-first string)
+  (if (string? string)(if (not (= (string-length string) 0)) (substring string 0 1) "string is too short") "not a string"))
+(string-first "test")
